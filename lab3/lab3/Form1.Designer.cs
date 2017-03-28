e4a3cbd3258c345f697a9d158033c2b3db126975
logan11116 <logan11116@mail.ru>
2017-03-29 02:47:38 +0300

    Crearea lab.3 metode http

---
+++ lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(525, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Incepe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Get",
+            "Head",
+            "Post",
+            "Checker",
+            "Crawl"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 203);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(474, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(603, 158);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(474, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "DATA";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(75, 229);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nr. cimpuri";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 250);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lab3/lab3/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+
+    {
+        string Url = "http://facebook.com";
+        string postUrl = "http://www.contoso.com/PostAccepter.aspx ";
+        string postData = "This is a test that posts this string to a Web server.";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 1;
+            textBox1.Text = "http://facebook.md/";
+          //  textBox3.Visible = false;
+            textBox3.Text = "10";
+          //  label3.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            if (comboBox1.SelectedIndex == 2)
+            {
+                Url = textBox1.Text;
+                textBox1.Text = postUrl;
+                textBox2.Text = postData;
+                label1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = false;
+                label3.Visible = false;
+            }
+            else if (comboBox1.SelectedIndex == 0)
+            {
+               
+                    textBox1.Text = Url;
+                    label2.Visible = false;
+                    textBox2.Visible = false;
+                    textBox3.Visible = false;
+                    label3.Visible = false;
+                
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+               
+                    textBox1.Text = Url;
+                    textBox3.Visible = true;
+                    label3.Visible = true;
+                label2.Visible = false;
+                textBox2.Visible = false;
+                     
+                
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+
+                textBox1.Text = Url;
+                textBox3.Visible = false;
+                label3.Visible = false;
+                textBox2.Visible = false;
+                label2.Visible = false;
+
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+
+                textBox1.Text = Url;
+                textBox3.Visible = false;
+                label3.Visible = false;
+                textBox2.Visible = false;
+                label2.Visible = false;
+
+            }
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.StartsWith("http://") && !textBox1.Text.StartsWith("ftp:/") && !textBox1.Text.StartsWith("https://")) textBox1.Text = "http://" + textBox1.Text;
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create(textBox1.Text);
+                    request.Credentials = CredentialCache.DefaultCredentials;
+                    WebResponse response = request.GetResponse();
+                    richTextBox1.AppendText("Status " + ((HttpWebResponse)response).StatusDescription + "\n\n");
+                    Stream dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    richTextBox1.AppendText(reader.ReadToEnd());
+                    reader.Close();
+                    response.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create(textBox1.Text);
+                    request.Credentials = CredentialCache.DefaultCredentials;
+                    request.Method = "HEAD";
+                    WebResponse response = request.GetResponse();
+                    richTextBox1.AppendText("Status " + ((HttpWebResponse)response).StatusDescription + "\n\n");
+                    richTextBox1.AppendText(response.Headers.ToString());
+                    response.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                try
+                {
+                    postData = textBox2.Text;
+                    WebRequest request = WebRequest.Create(postUrl);
+                    request.Method = "POST";
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                    request.ContentType = "application/x-www-form-urlencoded"; //multipart/form-data  
+                    request.ContentLength = byteArray.Length;
+                    Stream dataStream = request.GetRequestStream();
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    dataStream.Close();
+                    WebResponse response = request.GetResponse();
+                    richTextBox1.AppendText("Status " + ((HttpWebResponse)response).StatusDescription + "\n\n");
+                    dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    richTextBox1.AppendText(reader.ReadToEnd());
+                    reader.Close();
+                    dataStream.Close();
+                    response.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                try
+                {
+                    if (richTextBox1.Text != "") richTextBox1.Text = "";
+                    if (!textBox1.Text.StartsWith("http://") && !textBox1.Text.StartsWith("ftp:/") && !textBox1.Text.StartsWith("https://")) textBox1.Text = "http://" + textBox1.Text;
+                    if (label2.Text != "Result:") label2.Text = "Result:";
+                    WebRequest request = WebRequest.Create(textBox1.Text);
+                    WebResponse response = request.GetResponse();
+                    Stream stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream);
+                    string status = ((HttpWebResponse)response).StatusDescription;
+                    richTextBox1.Text = "Checking>.. " + textBox1.Text + " Status " + status + "\n";
+
+                    string pattern = @"(((http|https|ftp)+\:/\/)[&#95;a-z0-9\/&#95;:@=.+?,##%&~-]*[^.|\'|""|\# |!|\(|?|,| |>|<|;|\)])";
+
+                    MatchCollection numberoflinks = Regex.Matches(reader.ReadToEnd(), pattern, RegexOptions.Singleline);
+                    label2.Text = "Results found " + numberoflinks;
+
+                    int counter = 0;
+                    foreach (Match m in numberoflinks)
+                    {
+                        string link = m.Groups[1].Value;
+                        try
+                        {
+                            WebRequest newrequest = WebRequest.Create(link);
+                            WebResponse newresponse = newrequest.GetResponse();
+                            richTextBox1.AppendText("\n" + link + "  Status " + ((HttpWebResponse)newresponse).StatusCode);
+                            newresponse.Close();
+                            counter++;
+                            label2.Text = "Results found " + counter;
+                        }
+                        catch (Exception ex)
+                        {
+                            richTextBox1.AppendText("\n" + link + " " + ex.Message);
+                            counter++;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+
+            }
+
+            else if (comboBox1.SelectedIndex == 4)
+            {
+              //  textBox3.Visible = true;
+               // label3.Visible = true;
+                if (!textBox1.Text.StartsWith("http://") && !textBox1.Text.StartsWith("ftp://") && !textBox1.Text.StartsWith("https://")) textBox1.Text = "http://" + textBox1.Text;
+                for (int i = 0; i < Convert.ToInt32(textBox3.Text); i++)
+                {
+                    WebRequest newRequest = WebRequest.Create(Url);
+                    WebResponse newResponse = newRequest.GetResponse();
+                    Stream newStream = newResponse.GetResponseStream();
+                    StreamReader newReader = new StreamReader(newStream);
+                    richTextBox1.AppendText("\n" + Url + "  Status " + ((HttpWebResponse)newResponse).StatusCode);
+
+                    string pattern = @"(((http|https|ftp)+\:/\/)[&#95;a-z0-9\/&#95;:@=.+?,##%&~-]*[^.|\'|""|\# |!|\(|?|,| |>|<|;|\)])";
+                    MatchCollection links = Regex.Matches(newReader.ReadToEnd(), pattern, RegexOptions.Singleline);
+
+                index:
+                    Random rand = new Random();
+                    Url = links[rand.Next(links.Count)].Value;
+                    if (((HttpWebResponse)newResponse).StatusCode.ToString() != "OK") goto index;
+
+                    newResponse.Close();
+                    newReader.Close();
+
+
+
+                }
+            }
+        }
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
